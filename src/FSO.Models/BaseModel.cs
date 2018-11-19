59f7f8ede0a88899b9e7e137040700816877d042
892513490 <892513490@qq.com>
2018-11-19 22:27:45 +0800

    新增实体

---
+++ src/FSO.Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FSO.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
---
+++ src/FSO.Models/UrlInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FSO.Models
+{
+    [Table("T_UrlInfo")]
+    public class UrlInfo: BaseModel
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Remark { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public DateTime? ModifyDate { get; set; }
+    }
+}
