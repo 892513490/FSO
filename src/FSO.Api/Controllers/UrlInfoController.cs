b1623130c2f6d63d92da47ebc08d6a6e308a1c0b
892513490 <892513490@qq.com>
2018-11-19 22:34:29 +0800

    Api

---
+++ src/FSO.Api/Controllers/UrlInfoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FSO.Models;
+using FSO.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FSO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlInfoController : ControllerBase
+    {
+        private IUrlInfoService _service;
+
+        public UrlInfoController(IUrlInfoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetUrlInfos")]
+        public IList<UrlInfo> GetUserInfos()
+        {
+            return _service.GetList();
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(UrlInfo urlInfo)
+        {
+            _service.Add(urlInfo);
+            return Ok();
+        }
+
+        [HttpPost("Add")]
+        public IActionResult AddRang(IList<UrlInfo> urlInfos)
+        {
+            _service.Add(urlInfos);
+            return Ok();
+        }
+
+        [HttpGet("Delete/{id}")]
+        public IActionResult Delete(long id)
+        {
+            _service.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet("Get/{id}")]
+        public UrlInfo Get(long id)
+        {
+            return _service.Get(id);
+        }
+
+        public IActionResult Update(UrlInfo urlInfo)
+        {
+            _service.Update(urlInfo);
+            return Ok();
+        }
+    }
+}
--- src/FSO.Api/Startup.cs
+++ src/FSO.Api/Startup.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FSO.Models;
+using FSO.Repository;
+using FSO.Repository.Implements;
+using FSO.Service;
+using FSO.Service.Implements;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -22,10 +31,34 @@ namespace FSO.Api
 
         public IConfiguration Configuration { get; }
 
+        public IContainer ApplicationContainer { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContextPool<MySqlContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc().AddJsonOptions(options=> {
+                options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            
+
+            // Create the container builder.
+            var builder = new ContainerBuilder();
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            builder.RegisterType<UrlRepository<UrlInfo>>().As<IUrlRepository<UrlInfo>>();
+            builder.RegisterType<UserInfoService>().As<IUserInfoService>();
+            builder.RegisterType<UrlInfoService>().As<IUrlInfoService>();
+
+            //var assembly = System.Reflection.Assembly.GetEntryAssembly();
+            //builder.RegisterAssemblyTypes(assembly);
+
+            builder.Populate(services);
+            this.ApplicationContainer = builder.Build();
+
+            // Create the IServiceProvider based on the container.
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +72,8 @@ namespace FSO.Api
             {
                 app.UseHsts();
             }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//这是为了防止中文乱码
 
             app.UseHttpsRedirection();
             app.UseMvc();
