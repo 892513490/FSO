c59bfa7f974619c10a5e1195b2d56b9d5fe9d5a5
892513490 <892513490@qq.com>
2018-11-25 17:23:17 +0800

    新增video模块

---
+++ src/FSO.Web/Controllers/VideoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FSO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FSO.Web.Controllers
+{
+    public class VideoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Get(long id)
+        {
+            return View(new VideoInfo() {
+                Id = id,
+                Author = "杨老师",
+                Remark = "杨老师。。。。。。。。。。。。。",
+                ImgUrl = "",
+                Url = id % 2 == 1 ? "/upload/video/AngryBall.mp4" : "/upload/video/StreetBattle.mp4"
+            });
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
