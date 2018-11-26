e35995886d9ac59bf83990ee588fad110286473d
892513490 <892513490@qq.com>
2018-11-27 00:13:30 +0800

    1.新增视频上传 2.新增文件上传controller

---
+++ src/FSO.Web/Controllers/FileController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FSO.Web.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public FileController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upload()
+        {
+            var list = new List<IFormFile>();
+            string fileType = Request.Form["UploadType"];
+            if (string.IsNullOrWhiteSpace(fileType))
+            {
+                return BadRequest("错误的上传类型");
+            }
+            var files = Request.Form.Files;
+            string rootPath = _hostingEnvironment.WebRootPath;
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    string fileExt = formFile.FileName.Substring(formFile.FileName.LastIndexOf(".")); //文件扩展名，不含“.”
+                    long fileSize = formFile.Length; //获得文件大小，以字节为单位
+                    string newFileName = Guid.NewGuid().ToString() + "." + fileExt; //随机生成新的文件名
+                    var filePath = rootPath + "/upload/" + newFileName;
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+                list.Add(formFile);
+            }
+            return Ok(list);
+        }
+    }
+}
--- src/FSO.Web/Controllers/VideoController.cs
+++ src/FSO.Web/Controllers/VideoController.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using FSO.Models;
 using FSO.Service;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FSO.Web.Controllers
 {
     public class VideoController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
         private readonly IVideoInfoService _service;
 
-        public VideoController(IVideoInfoService service)
+        public VideoController(IHostingEnvironment hostingEnvironment,IVideoInfoService service)
         {
+            _hostingEnvironment = hostingEnvironment;
             _service = service;
         }
 
@@ -32,9 +37,48 @@ namespace FSO.Web.Controllers
             return View(videoInfo);
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(VideoInfo videoInfo)
+        {
+            var files = Request.Form.Files;
+            if (files.Count > 0)
+            {
+                var rootPath = _hostingEnvironment.ContentRootPath.Replace("\\","/") + "/wwwroot";
+                foreach (var formFile in files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        string fileExt = formFile.FileName.Substring(formFile.FileName.LastIndexOf(".")); //文件扩展名，不含“.”
+                        long fileSize = formFile.Length; //获得文件大小，以字节为单位
+                        string newFileName = Guid.NewGuid().ToString() + "." + fileExt; //随机生成新的文件名
+                        var filePath = rootPath;
+                        if (formFile.Name.Equals("fileImg"))
+                        {
+                            videoInfo.ImgUrl = "/upload/images/" + newFileName;
+                            filePath += videoInfo.ImgUrl;
+                        }
+                        else
+                        {
+                            videoInfo.Url = "/upload/video/" + newFileName;
+                            filePath += videoInfo.Url;
+                            videoInfo.IsLink = false;
+                        }
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+            }
+            
+            _service.Add(videoInfo);
+            return RedirectToAction("Index");
         }
     }
 }
