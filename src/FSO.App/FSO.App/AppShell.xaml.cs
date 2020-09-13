313c0a115ab0e27f62f0ff7dbc41f135bf6ad4e4
华南★飞虎 <892513490@qq.com>
2020-09-13 13:52:21 +0800

    add app

---
+++ src/FSO.App/FSO.App/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FSO.App.ViewModels;
+using FSO.App.Views;
+using Xamarin.Forms;
+
+namespace FSO.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ src/FSO.App/FSO.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using FSO.App.ViewModels;
+
+namespace FSO.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
