237763b75ddf26fbf3d294dd36042765575136b2
华南★飞虎 <892513490@qq.com>
2020-09-13 16:13:16 +0800

    add phone

---
+++ src/FSO.App/FSO.App/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FSO.App.ViewModels
+{
+    public class IndexViewModel : BaseViewModel
+    {
+        public IndexViewModel()
+        {
+            Title = "Index";
+        }
+    }
+}
